9eefddf1343906693a0ee9c4063628415e4990bb
vikash kumar <vk1395631@gmail.com>
2024-07-23 16:58:19 +0530

    conneted to mongo data base

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app=express();
+
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log('Connected to MongoDB');
+}).catch(err => console.log('Error connectinng to MongoDB'));
+
+app.get('/', (req, res) => {res.send(" i am responsing");});
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});
