b514d52f9cae23e9d537d4f0ee13e17de37885d1
vikash kumar <vk1395631@gmail.com>
2024-08-13 21:16:27 +0530

    "created User Model"

---
+++ api/Model/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema=mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+        
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    }
+},{timestamps:true});
+
+// timestamps ->for sorting the data using latest or ordest.
+const User=mongoose.model('User',userSchema);
+
+
+export default User;
