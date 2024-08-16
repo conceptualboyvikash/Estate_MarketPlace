2d68700aadb425795b0f2b8b69d7c1c8a52a8ded
vikash kumar <vk1395631@gmail.com>
2024-08-16 21:03:07 +0530

    adding redux persist in application

--- clinet/src/main.jsx
+++ clinet/src/main.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { store } from './redux/store.js'
+import { persistor,store } from './redux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
     // here we are making a store  for redux
-     <Provider store={store}>
-    
-    <App/>
-    </Provider>
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+
+        
+  </Provider>
+     /* The PersistGate component from Redux Persist is used to delay the rendering of your application until the Redux store has been rehydrated (i.e., the state has been restored from storage) */
   
 )
--- clinet/src/redux/store.js
+++ clinet/src/redux/store.js
@@ -1,12 +1,53 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore ,combineReducers} from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const rootReducer = combineReducers({ user: userReducer });
+/* combineReducers is used to combine multiple reducers into one root reducer.const rootReducer = combineReducers({
+  partA: reducerA,
+  partB: reducerB,
+}); like that */
+
+/* Redux Persist is a library that helps you save and rehydrate (restore) the Redux store's state between page reloads.
+*/
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+/*
+persistConfig specifies how Redux Persist should handle the state.
+key: 'root' is the key used in local storage to save the state.
+storage refers to the storage mechanism (in this case, local storage) where the state will be persisted.
+version helps in handling migrations if you decide to change the shape of the state in the future.
+
+version means differnt app version ..1.0.1 ,1.0.2 like that
+*/
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+//persistReducer -> high order reducer which wrap root reducer to make persistent
 export const store = configureStore({
   // { user: userReducer } is a slice of reducers which work as a reducer and acces like state.user.
-  reducer: { user: userReducer },
+  reducer:persistedReducer,
   //this middleware is a syntax so that we don't get error in future no meansing.
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+/* 
+configureStore creates the Redux store with the persistedReducer. This means that the state managed by this store will be saved to and restored from local storage.
+getDefaultMiddleware({ serializableCheck: false }) is used to configure middleware. The serializableCheck: false option is needed because Redux Persist uses non-serializable values (like local storage), which would otherwise cause warnings or errors.
+
+*/
+
+export const persistor = persistStore(store);
+
+/* 
+persistStore creates a persistor that can be used to subscribe to changes in the Redux store and rehydrate the state when the application is reloaded.
+*/
