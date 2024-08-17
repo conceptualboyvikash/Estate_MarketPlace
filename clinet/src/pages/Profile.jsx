22a566a8bb023fd0adec4bf2297447486bac9b64
vikash kumar <vk1395631@gmail.com>
2024-08-17 11:53:06 +0530

    complete image upload functionality

--- clinet/src/pages/Profile.jsx
+++ clinet/src/pages/Profile.jsx
@@ -1,17 +1,125 @@
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux';
+import { useRef, useState, useEffect } from 'react';
+
+//useRef is a hook use to triger same element with  other dom element 
+
+import{
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
+
+/*
+getDownloadURL: Retrieves the download URL for a file stored in Firebase Storage.
+getStorage:Initializes and returns an instance of Firebase Storage.
+ref:Creates a reference(References are like file paths, allowing you to point to a specific file or directory in your storage bucket) to a specific location in your Firebase Storage.
+
+uploadBytesResumable:Uploads a file to Firebase Storage and provides the ability to monitor the upload progress and resume the upload if interrupted.
+*/
 
 export default function Profile() {
-  const {currentUser} = useSelector((state) => state.user)
+
+  const fileRef = useRef(null);
+  const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  // firebase storage
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    /* 
+    uploadBytesResumable function in Firebase returns an UploadTask object. This object represents the ongoing upload process and provides methods and properties to monitor and control the upload.
+    */
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+
+ 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
-        <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg'  />
-        <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg'  />
-        <input type="text" placeholder='password' id='password' className='border p-3 rounded-lg'  />
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>update</button>
-
+      <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type='file'
+          ref={fileRef} // this imput elelment is ref using file ref 
+          hidden
+          accept='image/*'
+        />
+        {/* {accept='image/*' means only image files are allowed} */}
+        <img
+          onClick={() => fileRef.current.click()}
+          // this indirectly clickk that file
+          src={formData.avatar || currentUser.avatar}
+          alt='profile'
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
+        <p className='text-sm self-center'>
+          {fileUploadError ? (
+            <span className='text-red-700'>
+              Error Image upload (image must be less than 2 mb)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-700'>Image successfully uploaded!</span>
+          ) : (
+            ''
+          )}
+        </p>
+        <input
+          type='text'
+          placeholder='username'
+          id='username'
+          className='border p-3 rounded-lg'
+        />
+        <input
+          type='email'
+          placeholder='email'
+          id='email'
+          className='border p-3 rounded-lg'
+        />
+        <input
+          type='text'
+          placeholder='password'
+          id='password'
+          className='border p-3 rounded-lg'
+        />
+        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
+          update
+        </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className='text-red-700 cursor-pointer'>Delete account</span>
