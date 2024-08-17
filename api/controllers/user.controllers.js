3afc824515bd3ed81190ccd94cd2db64e62c928d
vikash kumar <vk1395631@gmail.com>
2024-08-17 18:54:57 +0530

    show listing functianl ity completed.

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -1,11 +1,23 @@
 import bcrypt from 'bcrypt'
 import { errorHandler } from '../utils/error.js';
 import User from '../Model/user.model.js';
+import Listing from '../Model/listing.model.js';
 
 export const test=(req, res) => {
     res.json({"name":"viaksh kuamr"})
 };
-
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
+  }
+};
 export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.id)
       return next(errorHandler(401, 'You can only update your own account!'));
--- api/router/user.router.js
+++ api/router/user.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, test, updateUser } from '../controllers/user.controllers.js';
+import { deleteUser, test, updateUser ,getUserListings} from '../controllers/user.controllers.js';
 import { verifyToken } from '../utils/verifyUser.js';
 //note yaha par .js important hai kuki file hamlog .user.controllesr karke like hai. okay
 
@@ -10,6 +10,7 @@ const userRouter=express.Router();
 userRouter.get('/test',test);
 userRouter.post('/update/:id', verifyToken, updateUser)
 userRouter.delete('/delete/:id', verifyToken, deleteUser)
+userRouter.get('/listings/:id', verifyToken, getUserListings)
 
 export default userRouter;
 
