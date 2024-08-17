85289a01f6b43e3a5b48607b75f20d855a6619e6
vikash kumar <vk1395631@gmail.com>
2024-08-17 15:50:25 +0530

    created create listnign page ui

--- clinet/src/pages/Profile.jsx
+++ clinet/src/pages/Profile.jsx
@@ -21,6 +21,7 @@ import {
   signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 /*
 getDownloadURL: Retrieves the download URL for a file stored in Firebase Storage.
@@ -207,6 +208,9 @@ export default function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
