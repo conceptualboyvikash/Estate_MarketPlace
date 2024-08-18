326f3ce336db21d835b3d2b4ee921c8c6f766896
vikash kumar <vk1395631@gmail.com>
2024-08-18 12:10:57 +0530

    ADDED SHOW MORE LISTING FUNCATIONLAITY

--- clinet/src/pages/Search.jsx
+++ clinet/src/pages/Search.jsx
@@ -17,6 +17,7 @@ export default function Search() {
   console.log(sidebardata);
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
+  const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -49,9 +50,16 @@ export default function Search() {
     }
     const fetchListings = async () => {
         setLoading(true);
+        setShowMore(false);
         const searchQuery = urlParams.toString();
         const res = await fetch(`/api/listing/get?${searchQuery}`);
         const data = await res.json();
+        console.log('frome on fetch listing okay',data.length);
+          if (data.length > 8) {
+            setShowMore(true);
+          } else {
+            setShowMore(false);
+          }
         setListings(data);
         setLoading(false);
       };
@@ -92,7 +100,20 @@ export default function Search() {
         setSidebardata({ ...sidebardata, sort, order });
       }
     };
-  
+    const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        console.log('frome on show more chlick',data.length);
+        if (data.length < 9) {
+          setShowMore(false);
+        }
+        setListings([...listings, ...data]);
+    };
     const handleSubmit = (e) => {
       e.preventDefault();
       const urlParams = new URLSearchParams();
@@ -202,6 +223,14 @@ export default function Search() {
             listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
             ))}
+            {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className='text-green-700 hover:underline p-7 text-center w-full'
+            >
+              Show more
+            </button>
+          )}
         </div>
       </div>
     </div>
