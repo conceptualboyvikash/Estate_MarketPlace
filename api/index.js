9304b40363a1f613ae56b70cb90bc45f242c5a02
vikash kumar <vk1395631@gmail.com>
2024-08-18 15:28:39 +0530

    i cnage the name to client

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import authRouter from './router/auth.router.js';
 import listingRouter from './router/listing.router.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-
+let port=5178;
 dotenv.config();
 
 const app=express();
@@ -15,16 +15,17 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-const __dirname = path.resolve();
 // dir name to get __dirnae of server.
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
     console.log('Connected to MongoDB');
 }).catch(err => console.log('Error connectinng to MongoDB'));
 
+const __dirname = path.resolve();
+
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing', listingRouter);
-let port=5178;
+
 
 app.use(express.static(path.join(__dirname, '/client/dist/')));
 //dist is a file name which is built after building app. and it will move those fronted router on this router.
@@ -35,14 +36,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/dist/index.html'));
   });
 */
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'clinet', 'dist', 'index.html'));
-})
-app.use((err, req, res, next) => {
-    
-    const message = err.message||"Internal server error";
-    const statusCode=err.statusCode || 500;
-    return res.status(statusCode).json({success:false,statusCode,message});
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 app.listen(port,()=>{
     console.log(`Server is running on ${port}`);
