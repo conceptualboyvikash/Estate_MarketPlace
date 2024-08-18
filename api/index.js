d21c1c9dda9cc72b76ab638fc75980c52f172533
vikash kumar <vk1395631@gmail.com>
2024-08-18 13:06:42 +0530

    added prerendering essenstial

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './router/user.router.js';
 import authRouter from './router/auth.router.js';
 import listingRouter from './router/listing.router.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -14,7 +15,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-
+const __dirname = path.resolve();
+// dir name to get __dirnae of server.
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
     console.log('Connected to MongoDB');
 }).catch(err => console.log('Error connectinng to MongoDB'));
@@ -23,6 +25,18 @@ app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+//dist is a file name which is built after building app. and it will move those fronted router on this router.
+/*
+like 
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, '/client/dist/index.html'));
+  });
+*/
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 app.use((err, req, res, next) => {
     
     const message = err.message||"Internal server error";
