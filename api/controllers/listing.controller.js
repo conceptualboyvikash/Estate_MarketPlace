1575ddc67db2f5d2be417d50329765c441fae2a0
vikash kumar <vk1395631@gmail.com>
2024-08-18 11:12:55 +0530

    add onchange and submit funcationlity to search page

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -60,3 +60,63 @@ export const getListing = async (req, res, next) => {
   }
   
 };
+
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === 'false') {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === 'false') {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sale', 'rent'] };
+    }
+
+    const searchTerm = req.query.searchTerm || '';
+    // means what we write in serch boxx okay
+    const sort = req.query.sort || 'createdAt';
+    // here short with which paramerter.
+
+    const order = req.query.order || 'desc';
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: 'i' },
+      //here  $options: 'i' to  case insensitive name &regex match the resut whose "name " start with searchTerm okay.
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      //sort variable representing the name of the field by which you want to sort the documents. order means which order ascd or desc order =-1/"desc" for descnding and 1/"asc" for ascening.
+      .limit(limit)
+      .skip(startIndex);
+
+     /* .limit(limit): Limits the number of documents returned to the specified limit.
+      .skip(n): Skips the first n number of documents and returns the rest.
+      Together, they are used for pagination—retrieving a specific page of results from a larger dataset.
+    */
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
--- clinet/src/components/Header.jsx
+++ clinet/src/components/Header.jsx
@@ -1,10 +1,35 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux';
+import { Link ,useNavigate} from "react-router-dom";
+import { useSelector} from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    //first we make a new url instatance useing URLSearchParams for (adding, updating, or removing )
+    //window.location.search this gives the current serach term;
+
+    urlParams.set('searchTerm', searchTerm);
+    //here if in url we have search term then its value will be updated with serachterm state. orderwise  serachterm=serachterm added in url.
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  // window.location returns current url where as window.location.serach return qurey stirng of current url  i.e after '? '
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+   // here getting the value of serachTerm and if value then update that
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  //location.search is same as window.locaiton.serach
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -15,12 +40,15 @@ export default function Header() {
           </h1>
         </Link>
         <form
+          onSubmit={handleSubmit}
           className="bg-slate-100 p-3 rounded-lg flex items-center"
         >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button>
             <FaSearch className="text-slate-600" />
